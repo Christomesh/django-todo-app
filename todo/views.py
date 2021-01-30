49be85c52542f7399c7aeff31bd2ffd5be164152
Christomesh <meshacholuwadamilare@gmail.com>
2021-01-30 04:03:07 +0100

    list todo

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
+from .models import Todo
 # Create your views here.
 
 def home(request):
@@ -53,5 +54,7 @@ def createtodo(request):
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error':"Bad data passed In, Try again."})
+
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos':todos})
